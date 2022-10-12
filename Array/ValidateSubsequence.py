6f81e277cfcda381ffd67db755002fe82f259240
Cayla Bradley <99041331+caynoelani@users.noreply.github.com>
2022-10-12 15:05:26 -0500

    Annotate ValidSubsequence.py

--- Array/ValidateSubsequence.py
+++ Array/ValidateSubsequence.py
@@ -1,7 +1,19 @@
 # Given two non-empty arrays of integers, write a function that determines whether the second array is a subsequence of the first one. A subsequence of an array is a set of numbers that aren't necessarily adjacent in the array, but that are in the same order as they appear in the array. For instance, [1, 3, 4] is a subsequence of [1, 2, 3, 4]
 
-def ValidSubsequence(array1, sequence1):
-    return
+def ValidSubsequence(array, subsequence): #takes in two arrays
+    i = 0 #iterates through array
+    j = 0 #iterates through subsequence
+
+    while j < len(subsequence) and i < len(array): #while value is not undefined
+        if array[i] == subsequence[j]: #check is value is in subsequence and in same order
+            j += 1 #if so, increment j
+
+        i +=1 #increment i
+        
+    if j == len(subsequence): #if we traversed through entire subsequence
+        return True #is valid subsequence
+    else: #otherwise
+        return False #is not valid subsequence
 
 array1 = [5, 1, 22, 25, 6, -1, 8, 10]
 sequence1 = [1, 6, -1, 10]
